49f8666d77dd9aae5aa4cbecc469609058d97c6c
Ginger Ciaburri <87339867+ggciab@users.noreply.github.com>
2023-04-22 23:05:33 -0400

    Add upload file ability

--- process_letters.py
+++ process_letters.py
@@ -1,20 +1,36 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import spacy
 import ner
 import db
+from werkzeug.utils  import secure_filename
+import os
 
 app = Flask(__name__)
 nlp = spacy.load("en_core_web_sm")
 connection = db.DatabaseConnection('entities.sqlite')
 my_markup_dict = {'mark': "Please return to the home page to add your input."}
 
+IMAGE_DIR = "templates/UPLOADED_DOCS/"
+os.makedirs(IMAGE_DIR, exist_ok = True)
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('form.html')
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            dir_file_name = IMAGE_DIR + uploaded_file.filename
+            uploaded_file.save(dir_file_name)
+        return redirect(url_for('show_result', file=dir_file_name))
+    return render_template('home_page.html')
+
 
 @app.route('/analyze', methods=["GET", "POST"])
 def show_result():
+    file = request.args.get('file')
+    print(file)
+    ## get text from pdf/jpeg
+    # this may need to be split into multiple methods
+    ## show image and a text box for editting the text we find
     if request.method == "POST":
         text = request.form["text_input"]
         if text:
@@ -37,11 +53,11 @@ def show_result():
                     cursor.execute("UPDATE entities SET count = ? WHERE label = ? AND text = ?", (count, ent.label_, ent.text))
             connection.commit()
             my_markup_dict['mark'] = ner_spacyed_text
-            return render_template("result.html", text=ner_spacyed_text, entities=doc.ents)
+            return render_template("result.html", text=ner_spacyed_text, entities=doc.ents, imgage_file=file)
         else:
-            return render_template('result.html', text=my_markup_dict['mark'])  # To prompt user to add input
+            return render_template('result.html', text=my_markup_dict['mark'], imgage_file=file)  # To prompt user to add input
     else:
-        return render_template('result.html', text=my_markup_dict['mark'])  # To prompt user to add input
+        return render_template('result.html', text=my_markup_dict['mark'], imgage_file=file)  # To prompt user to add input
 
 @app.route('/entity_db')
 def get_counts():
